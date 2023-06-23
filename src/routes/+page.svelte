8d4def4092bf8a8587446cd1e6e4e20e598ea9cd
Owen <owenventer99@gmail.com>
2023-06-23 08:00:09 +0200

    Updated UI of webpage to git to the Figma design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<div class="mx-auto max-w-xl py-10">
-    <h1 class="text-8xl font-bold">Home Page</h1>
-    <p>This is a page</p>
-    <button class="btn">Button</button>
-    <button class="btn-primary btn">Button</button>
-    <button class="btn-secondary btn">Button</button>
+<div class="min-h-screen bg-white">
+    <div class="mx-auto max-w-xl py-10">
+        <h1 class="text-8xl font-bold">Home Page</h1>
+        <p>This is a page</p>
+        <button class="btn">Button</button>
+        <button class="btn-primary btn">Button</button>
+        <button class="btn-secondary btn">Button</button>
+    </div>
 </div>
