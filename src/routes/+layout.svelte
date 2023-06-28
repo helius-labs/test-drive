5cdbb4cd7bb2defaa1256c385f4a24786bc812a8
Owen <owenventer99@gmail.com>
2023-06-28 22:34:54 +0200

    added link to dev portal button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -80,7 +80,7 @@
             class="flex items-center rounded border border-cyan-600 bg-HeliusOrange p-2 text-center font-semibold leading-normal text-white"
         >
             <a
-                href="test"
+                href="https://dev.helius.xyz/dashboard/app"
                 class="link no-underline"
             >
                 Launch Dev Portal
