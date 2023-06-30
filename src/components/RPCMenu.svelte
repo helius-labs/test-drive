cf71e2be706cd6f8640f7b34478106a06baeee77
Owen <owenventer99@gmail.com>
2023-06-30 11:54:35 +0200

    added responseStore and connected to response component

--- src/components/RPCMenu.svelte
+++ src/components/RPCMenu.svelte
@@ -2,6 +2,7 @@
     // @ts-nocheck
     import { methods } from "$lib/api/methods.js";
     import { currentMethod } from "$lib/stores/currentMethodStore.js";
+    import { responseStore } from "$lib/stores/responseStore.js";
 
     let selected;
 
@@ -32,8 +33,8 @@
     }
     async function callRPC(method, params) {
         let body = {
-            jsonrpc: "2.0",
             id: 1,
+            jsonrpc: "2.0",
             method,
         };
 
@@ -47,15 +48,16 @@
         }
 
         const response = await fetch(answer, {
-            method: "POST",
+            body: JSON.stringify(body),
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(body),
+            method: "POST",
         });
 
         const data = await response.json();
         console.log(data);
+        responseStore.set(JSON.stringify(data));
         return data.result;
     }
 </script>
@@ -71,7 +73,6 @@
             <div class="m-1 w-1/3">
                 <select
                     bind:value={$currentMethod}
-                    on:change={() => (answer = "")}
                     class=" w-full rounded bg-white p-2 text-black"
                 >
                     {#each questions as question}
@@ -84,7 +85,7 @@
             <div class="m-1 w-2/3">
                 <input
                     bind:value={answer}
-                    class="w-full rounded bg-white p-2 text-[#D3D6DC]"
+                    class="w-full rounded bg-white p-2 text-black"
                     placeholder="Drop a Solana endpoint here"
                 />
             </div>
--- src/components/Response.svelte
+++ src/components/Response.svelte
@@ -1,22 +1,5 @@
 <script>
-    let code = `{
-  "jsonrpc": "2.0",
-  "result": {
-    "meta": {
-      "err": null,
-      "fee": 5000,
-      "innerInstructions": [],
-      "postBalances": [499998932500, 26858640, 1, 1, 1],
-      "postTokenBalances": [],
-      "preBalances": [499998937500, 26858640, 1, 1, 1],
-      "preTokenBalances": [],
-      "rewards": [],
-      "status": {
-        "Ok": null
-      }
-    }
-  }
- }`;
+    import { responseStore } from "$lib/stores/responseStore.js";
 </script>
 
 <div class="m-5 p-2 text-black">
@@ -24,7 +7,8 @@
     <div class=" rounded border">
         <h1 class=" rounded-t bg-[#929498] p-2 text-lg font-semibold">JSON:</h1>
         <div class=" bg-[#282935] p-4 text-white">
-            <pre><code style="white-space: pre-wrap;">{code}</code></pre>
+            <pre><code style="word-break: break-all;">{$responseStore}</code
+                ></pre>
         </div>
         <div class=" flex items-center justify-between border p-2">
             <div class=" h-min rounded bg-green-500 p-2 text-center text-white">
